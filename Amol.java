a62e3c47b0db4be7cb025bd84100ffd51158301b
AMOL_GARGE <creatives.garje@gmail.com>
2023-07-30 14:53:36 +0530

    Amol-Update-2-Commite1

--- Amol.java
+++ Amol.java
@@ -4,5 +4,8 @@ public class Amol {
      public static void main(String[] args) {
 		System.out.println("I am Amol Garje");
 		System.out.println("Hellow Everyone Bye To All..");
+		for(int i=0;i<10;i++){
+                      System.out.println(i);
+		}
 	}
 }
