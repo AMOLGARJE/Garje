b12d542611f561a9becb9f641d42de2dea93f632
AMOL_GARGE <creatives.garje@gmail.com>
2023-07-30 14:32:38 +0530

    Second-Commite

--- Amol.java
+++ Amol.java
@@ -3,5 +3,6 @@ package Amol.Garje;
 public class Amol {
      public static void main(String[] args) {
 		System.out.println("I am Amol Garje");
+		System.out.println("Hellow Everyone Bye To All..");
 	}
 }
