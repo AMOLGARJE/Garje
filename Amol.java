0595b92f80ef9946ef73864bd873dae5c7b0911c
AMOL_GARGE <creatives.garje@gmail.com>
2023-07-30 14:23:42 +0530

    First-Commite

---
+++ Amol.java
@@ -0,0 +1,7 @@
+package Amol.Garje;
+
+public class Amol {
+     public static void main(String[] args) {
+		System.out.println("I am Amol Garje");
+	}
+}
---
+++ Atul.java
@@ -0,0 +1,7 @@
+package Amol.Garje;
+
+public class Atul {
+     public static void main(String[] args) {
+		System.out.println("I am Atul Kailas Garje...");
+	}
+}
